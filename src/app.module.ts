5711b16797eef3323122dd20347dfe1449127891
Nhat Ho <nhatho0001@gmail.com>
2025-04-07 10:40:52 +0700

    change app.module.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, CustomerModule],
   controllers: [AppController],
   providers: [AppService],
 })
